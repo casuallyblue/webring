8c17b094ce1a27ef456ead2519717163cfb08ba4
casually-blue <darkforestsilence@gmail.com>
2023-11-05 23:13:50 -0600

    Make images work

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let css_dir = ServeDir::new("css");
     let static_dir = ServeDir::new("static");
 
+    let images_dir = ServeDir::new("images");
+
     let app = Router::new()
         .nest_service("/js", js_dir)
         .nest_service("/css", css_dir)
         .nest_service("/static", static_dir)
+        .nest_service("/images", images_dir)
         .route("/", get(|| async { HomePage {}.render() }));
 
     let ip_port = format!("0.0.0.0:{}", options.port);
